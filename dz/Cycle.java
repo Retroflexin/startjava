029a3a46209135d515299c37c1c76b23ca7bd473
Retroflexin <45211595+Retroflexin@users.noreply.github.com>
2018-12-06 23:31:48 +0300

    Dopolnil

--- dz/Cycle.java
+++ dz/Cycle.java
@@ -2,6 +2,22 @@ public class Cycle {
 	public static void main(String[] args) {
 
 		int i = 0;
-		for (i != 20 do i+1);
+		for (i = 0; i <= 19; i++); {
+			System.out.println (i + " ");
+		}
+
+		int u = 6;
+        while (u >= -6) {
+           System.out.print (u + " ");
+           u--;
+        }
+
+         int o = 10;
+         int sum = 0;
+        do {
+            o++;
+            sum = o % 1;
+        } while(sum == 20);
+        System.out.println(sum);
 	}
 }
