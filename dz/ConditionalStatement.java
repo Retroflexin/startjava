a7beb2966e4d0d986545556b928cf77ccfbcc77c
Retroflexin <45211595+Retroflexin@users.noreply.github.com>
2018-12-07 20:06:02 +0300

    Отформатировал

--- dz/ConditionalStatement.java
+++ dz/ConditionalStatement.java
@@ -4,41 +4,41 @@ public class ConditionalStatement {
 
 	 	int age = 21;
 	 	if (age > 20) {
-	 		System.out.println("Vi starshe, chem" + age);
+	 	System.out.println("Vi starshe, chem" + age);
 	 	}
 
-	 		boolean male = true;
-	 		if (male == true) {
-	 			System.out.println("Vi muzhchina, congrats");
-	 		}
+	 	boolean male = true;
+	 	if (male == true) {
+	 	System.out.println("Vi muzhchina, congrats");
+	 	}
 
-	 		boolean female = true;
-	 		if (female == true) {
-	 			System.out.println("Vi zhenshina, congrats");
-	 		}
+	 	boolean female = true;
+	 	if (female == true) {
+	 	System.out.println("Vi zhenshina, congrats");
+	 	}
 
-	 		float rost = 1.75f;
-	 		float rost1 = 1.85f;
+	 	float rost = 1.75f;
+	 	float rost1 = 1.85f;
 
-	 		if (rost < 1.80 == false) {
-	 			System.out.println("Vash rost men'she, chem 180cm");
-	 		} else if (rost1 > 1.80 == true)  {
-	 			System.out.println("Vash rost bol'she, chem 180cm");
-	 		}
+	 	if (rost < 1.80 == false) {
+	 	System.out.println("Vash rost men'she, chem 180cm");
+	 	} else if (rost1 > 1.80 == true)  {
+	 	System.out.println("Vash rost bol'she, chem 180cm");
+	 	}
 
-	 		char M1 = 'M';
-	 		char I1 = 'I';
-	 		char T1 = 'T';
+	 	char M1 = 'M';
+	 	char I1 = 'I';
+	 	char T1 = 'T';
 	 		
-	 		if (M1 == I1) {
-	 		System.out.println("Vashe imya nachinaetsya s bukvi M");
-	 	    }
-	 		else if (I1 != M1) { 
-	 			System.out.println("Vashe imya nachinaetsya s bukvi I");
-	 		   }
-	 		   else if (T1 == T1) { 
-	 			System.out.println("Vashe imya nachinaetsya s bukvi T");
-	 		   }
+	 	if (M1 == I1) {
+	 	System.out.println("Vashe imya nachinaetsya s bukvi M");
+	 	}
+	 	else if (I1 != M1) { 
+	 	System.out.println("Vashe imya nachinaetsya s bukvi I");
+	    }
+	 	else if (T1 == T1) { 
+	 	System.out.println("Vashe imya nachinaetsya s bukvi T");
+	    }
 
-	 		}
-	 		}
+	 	}
+	 	}
