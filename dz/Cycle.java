1c4db62b31b55f617035a3c5e23ecde7ab50c946
Retroflexin <45211595+Retroflexin@users.noreply.github.com>
2018-12-07 20:07:19 +0300

    Я не понимаю, как вывести for и как сделать do while

--- dz/Cycle.java
+++ dz/Cycle.java
@@ -1,23 +1,26 @@
 public class Cycle {
 	public static void main(String[] args) {
 
-		int i = 0;
-		for (i = 0; i <= 19; i++); {
-			System.out.println (i + " ");
-		}
+	int i = 0;
+	for (i = 0; i <= 20; i++); {
+	System.out.println (i);
+	}
 
-		int u = 6;
-        while (u >= -6) {
-           System.out.print (u + " ");
-           u--;
-        }
+	int u = 6;
+    while (u >= -6) {
+    System.out.println (u + " ");
+    u--;
+    }
 
-         int o = 10;
-         int sum = 0;
-        do {
-            o++;
-            sum = o % 1;
-        } while(sum == 20);
-        System.out.println(sum);
+    int o = 10;
+    int sum = 0;
+    do {
+     if(o % 2 != 0); {
+     	System.out.println(sum + o);
+     	sum += o;
+     }
+    o++;
+    } while(o <= 20);
+    System.out.println(o + sum);
 	}
 }
